bba50069d6a3021c1e2e5300d32113f6e2dff3cd
BC <4169770+5cf2a7d4bf6e4cdb64b37b7a03b9f2f7@users.noreply.github.com>
2022-11-20 00:05:46 +0800

    fix hidden text reveal

--- assets/mod.js
+++ assets/mod.js
@@ -1,14 +1,30 @@
-// 
+// hidden text reveal
+function hasClass(ele, cls) {
+    return ele.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+}
+
+function addClass(ele, cls) {
+    if (!hasClass(ele, cls))
+        ele.className += " " + cls;
+}
+
+function removeClass(ele, cls) {
+    if (hasClass(ele, cls)) {
+        var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+        ele.className = ele.className.replace(reg, ' ');
+    }
+}
+
 function makeHiddenTextReveal(){
   const hiddenTexts = document.getElementsByClassName('hidden-text');
   for (const hiddenText of hiddenTexts) {
     hiddenText.setAttribute("showHidden", "0");
     hiddenText.addEventListener('click', function onClick(event) {
       if (event.target.getAttribute("showHidden") == "0") {
-        event.target.classList.remove = 'hidden-text';
+        removeClass(event.target, 'hidden-text');
         event.target.setAttribute("showHidden", "1");
       } else {
-        event.target.classList.add = 'hidden-text';
+        addClass(event.target, 'hidden-text');
         event.target.setAttribute("showHidden", "0");
       }
     });
