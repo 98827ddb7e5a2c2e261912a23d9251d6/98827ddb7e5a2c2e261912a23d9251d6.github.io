5e61b10e6cff5619560b11d5244752a44792b588
BC <4169770+5cf2a7d4bf6e4cdb64b37b7a03b9f2f7@users.noreply.github.com>
2022-11-20 23:43:57 +0800

    update photowall

--- assets/images.js
+++ assets/images.js
@@ -5,22 +5,28 @@ var imageList =
     "url": "https://pbs.twimg.com/media/FfMi5v1UAAE1b1k?format=jpg&name=large",
     "des": "Kigurumi, Dollkii 黑曜。Near Hong Kong-Zhuhai-Macao Bridge in ZhuHai.",
     "ref": "https://twitter.com/KEKI_1112/status/1581646304517984256",
-    "date": "2022-10-16 17:10:30"
+    "date": "2022-10-16 17:10:30",
   },
   {
     "url": "https://pbs.twimg.com/media/FdhAMpeVsAAfcjY?format=jpg",
     "des": "Rosewood GZ",
-    "date": "2022-09-26"
+    "date": "2022-09-26",
   },
   {
     "url": "https://pbs.twimg.com/media/FfqB3WCaUAAhxaC?format=jpg&name=large",
-    "des": "Kigurumi, Dollkii 南烛。CM Viking Cruise, SZ<->XM.",
+    "des": "Kigurumi, Dollkii 南烛。CM Viking Cruise, Shenzhen<->Xiamen 5 days cruise.",
     "ref": "https://twitter.com/KEKI_1112/status/1583721020800012288",
-    "date": "2022-08-21 12:33:18"
+    "date": "2022-08-21 12:33:18",
   },
   {
     "url": "https://pbs.twimg.com/media/Fb_Z6FhaUAc5JZu?format=jpg",
     "des": "CM Viking Cruise, balcony of ES2 suite.",
-    "date": "2022-08-20"
+    "date": "2022-08-20",
+  },
+  {
+    "url": "https://pbs.twimg.com/media/EaCPTWWUwAIDQIi?format=jpg",
+    "des": "Kigurumi, liildesign NANA-03. 御温泉 in Zhuhai.",
+    "ref": "https://twitter.com/KEKI_1112/status/1583721020800012288",
+    "date": "2019-01-09",
   },
 ];
