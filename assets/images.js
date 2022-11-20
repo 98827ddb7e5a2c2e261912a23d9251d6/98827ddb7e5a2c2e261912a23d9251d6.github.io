15079fd5915ee46878f66aad098aaad19f83f34e
BC <4169770+5cf2a7d4bf6e4cdb64b37b7a03b9f2f7@users.noreply.github.com>
2022-11-20 23:37:24 +0800

    prio disable zoom, update photowall

--- assets/images.js
+++ assets/images.js
@@ -3,7 +3,7 @@ var imageList =
 [
   {
     "url": "https://pbs.twimg.com/media/FfMi5v1UAAE1b1k?format=jpg&name=large",
-    "des": "黑曜。Near Hong Kong-Zhuhai-Macao Bridge in ZhuHai.",
+    "des": "Kigurumi, Dollkii 黑曜。Near Hong Kong-Zhuhai-Macao Bridge in ZhuHai.",
     "ref": "https://twitter.com/KEKI_1112/status/1581646304517984256",
     "date": "2022-10-16 17:10:30"
   },
@@ -14,7 +14,7 @@ var imageList =
   },
   {
     "url": "https://pbs.twimg.com/media/FfqB3WCaUAAhxaC?format=jpg&name=large",
-    "des": "南烛。CM Viking Cruise, SZ<->XM.",
+    "des": "Kigurumi, Dollkii 南烛。CM Viking Cruise, SZ<->XM.",
     "ref": "https://twitter.com/KEKI_1112/status/1583721020800012288",
     "date": "2022-08-21 12:33:18"
   },
