2da066a67cbb705c87ceabac6aa09ceb219eae47
BC <4169770+5cf2a7d4bf6e4cdb64b37b7a03b9f2f7@users.noreply.github.com>
2022-11-21 14:23:07 +0800

    try img source from adobe portfolio

--- assets/images.js
+++ assets/images.js
@@ -54,4 +54,31 @@ var imageList =
     "ref": "https://twitter.com/KEKI_1112/status/1014140290507030528",
     "date": "2018-06",
   },
+  {
+    "url": "https://cdn.myportfolio.com/70f34193-df85-4feb-9918-c95f6a8c3810/4a4eb810-daaf-41bb-8729-eeea64d12235_rw_1200.jpg?h=0177a89f72a365c1bdd5c496f6df46ac",
+    "des": "玉龙雪山。After rainstorm, taken with iPhone 4S.",
+    "ref": "",
+    "date": "2012-08-16",
+  },
+  {
+    "url": "https://cdn.myportfolio.com/70f34193-df85-4feb-9918-c95f6a8c3810/31f2acac-0adb-4f40-842f-0ea94f87ee47_rw_1920.jpg?h=81e98d549a46dd94f492fcdcb2dbd3a5",
+    "des": "Highschool chemistry class. Kodak Color Plus 200.",
+    "ref": "",
+    "date": "",
+  },
+  {
+    "url": "https://cdn.myportfolio.com/70f34193-df85-4feb-9918-c95f6a8c3810/44c0a468-6102-43f9-bb49-994a53de9f7f_rw_1920.jpg?h=9031215240783ba7ea9da71752d11076",
+    "des": "The first photo I took that I felt quite happy about.",
+    "date": "2010-08-02",
+  },
+  
 ];
+
+/*
+  {
+    "url": "",
+    "des": "",
+    "ref": "",
+    "date": "",
+  },
+*/
